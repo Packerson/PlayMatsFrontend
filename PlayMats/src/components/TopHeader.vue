6d8844ce91f2e9042b1955f9ea65179b7c51c23b
Pawel <pawel.91.kaczmarek@gmail.com>
2024-06-09 18:24:38 +0200

    TopHeader

---
+++ PlayMats/src/components/TopHeader.vue
@@ -0,0 +1,109 @@
+<template>
+  <q-header elevated>
+
+    <div class="first-row">
+
+      <div class="logo">
+        <span>Logo should be here</span>
+      </div>
+
+      <div>
+        <q-btn
+          flat
+          round
+          dense
+          label="PL"
+          icon="img:/flags/pl.svg"
+          class="q-mr-md"
+        />
+        <span class="q-mr-md">
+          <q-icon name="phone" />
+          Zadzwoń
+          <q-tooltip>
+            <span>123 456 789</span>
+          </q-tooltip>
+        </span>
+        <q-btn
+          flat
+          round
+          dense
+          label="Napisz"
+          icon="mail"
+        />
+      </div>
+    </div>
+
+    <div  class="second-row">
+      <div class="q-mx-md">
+        <q-btn
+          v-for="item in menu"
+          flat
+          round
+          dense
+          :label="item"
+          :key="item"
+          class="q-ml-md menu-item"
+        />
+      </div>
+
+    </div>
+
+
+  </q-header>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TopHeader',
+  setup() {
+
+    const topMenu = [
+      'Flaga/wybór języka', 'Telefon', 'Napisz',
+    ]
+    const menu = [
+      'Maty', 'Tereny', 'Karty', 'Żetony', 'Akcesoria',
+    ]
+    return {
+      menu,
+      topMenu,
+    }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+
+.q-header {
+  background: linear-gradient(123.2deg, #a8a5a5 15.09%, #E1E1E1 102.56%);
+  color: #000;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  .first-row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 20px;
+    .logo {
+    font-size: 1.5rem;
+    font-weight: bold;
+    }
+  }
+  .second-row {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: center;
+    gap: 20px;
+    .menu-item {
+      color: black;
+      font-size: 16px;
+    }
+  }
+}
+
+</style>
---
+++ PlayMats/src/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout class="layout-main" view="hHh Lpr fFf">
+    <TopHeader />
+    <div>
+      <h2>Play Mats</h2>
+    </div>
+    <Q-page-container>
+      <router-view />
+    </Q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+
+
+import { defineComponent } from 'vue'
+import  TopHeader  from 'src/components/TopHeader.vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+  components: {
+    TopHeader
+
+  },
+  setup() {
+    return {
+    }
+  }
+
+})
+</script>
+
+
+<style lang="scss">
+.layout-main {
+    background: linear-gradient(123.2deg, #a8a5a5 15.09%, #E1E1E1 102.56%);
+}
+
+</style>
