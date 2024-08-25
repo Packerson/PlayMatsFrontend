442bc44d973bfe8c542210a9461c211ef2b208ff
Pawel <pawel.91.kaczmarek@gmail.com>
2024-08-25 12:28:43 +0200

    update gitignore

--- PlayMats/src/secrets/email.js
+++
@@ -1,3 +0,0 @@
-export const service_id = 'service_krxo9za';
-export const template_id = 'template_kvk1xqb';
-export const user_id = 'jGNBquyB6ajwtMYri';
---
+++ PlayMats/src/secrets/emailEmpty.js
@@ -0,0 +1,3 @@
+export const service_id = '';
+export const template_id = '';
+export const user_id = '';
